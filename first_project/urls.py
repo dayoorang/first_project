13c6d52ba6e482551bdb512c2f4b2e41cb1b7fdd
yangsunkang <dayoorang@gmail.com>
2021-07-27 12:47:30 +0900

    7/27 first_project commit 4

--- first_project/urls.py
+++ first_project/urls.py
@@ -1,9 +1,12 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from first_project import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls'))
 
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # docutment_root 경로를 뒤져서 MEDIA_URL 경로로 입력해주겠다라는 뜻.
