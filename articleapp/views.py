6188d904142ded2923a5539880969724c2ad7c8a
yangsunkang <dayoorang@gmail.com>
2021-08-06 11:44:13 +0900

    8/6 first_project commit 2

--- articleapp/views.py
+++ articleapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
@@ -52,3 +52,11 @@ class ArticleDeleteView(DeleteView):
     context_object_name = 'target_article'
     success_url = reverse_lazy('articleapp:list')
     template_name = 'articleapp/delete.html'
+
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'   # 게시글의 다발을 들고있는 객체임.. 앞선 단일 객체랑은 다름..
+    template_name = 'articleapp/list.html'
+    paginate_by = 20
+
