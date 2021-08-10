4a80e08b19b4d2d385dc1604f30693193b768b57
yangsunkang <dayoorang@gmail.com>
2021-08-10 11:15:11 +0900

    8/10 first_project commit 1

---
+++ commentapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+
+# urlpatterns = [
+#
+#     path('comments/', include('commentapp.urls')),
+#
+#   ]
--- first_project/urls.py
+++ first_project/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
-    path('articles/', include('articleapp.urls'))
+    path('articles/', include('articleapp.urls')),
+    path('comments/', include('commentapp.urls')),
 
   ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # docutment_root 경로를 뒤져서 MEDIA_URL 경로로 입력해주겠다라는 뜻.
