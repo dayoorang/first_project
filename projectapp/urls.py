bd04b65bdbcf5b68e52b0af657415b509518d21e
yangsunkang <dayoorang@gmail.com>
2021-08-13 14:46:28 +0900

    8/13 first_project commit 5

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -3,8 +3,7 @@ from django.urls import path, include
 from projectapp.views import ProjectCreateView
 
 
-app_name = 'projectapp'
-
+app_name= 'projectapp'
 urlpatterns = [
-    path('create/', ProjectCreateView.as_view(), name='create')
+      path('create/', ProjectCreateView.as_view(), name= 'create')
   ]
