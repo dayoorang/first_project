7c928419d60f5ce5d09c2973173a293d1aca374a
yangsunkang <dayoorang@gmail.com>
2021-08-13 12:19:29 +0900

    8/13 first_project commit 4

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.views.generic import CreateView
+
+from projectapp.forms import ProjectCreationForm
+from projectapp.models import Project
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    form_class = ProjectCreationForm
+    success_url = reverse_lazy('articleapp:list')
+    template_name = 'projectapp/create.html'
