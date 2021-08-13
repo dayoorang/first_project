f5b87d44cc9c7b1a4f5a89393859d5b3dc9e3409
yangsunkang <dayoorang@gmail.com>
2021-08-13 11:42:02 +0900

    8/13 first_project commit 3

--- first_project/urls.py
+++ first_project/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('profiles/', include('profileapp.urls')),
     path('articles/', include('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
+    path('projects/', include('projectapp.urls')),
 
   ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # docutment_root 경로를 뒤져서 MEDIA_URL 경로로 입력해주겠다라는 뜻.
