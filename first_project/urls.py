2d614d65d824e52f73096744660d7c6adf564eaf
yangsunkang <dayoorang@gmail.com>
2021-08-20 16:41:44 +0900

    8/20 first_project commit 1

--- first_project/urls.py
+++ first_project/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('articles/', include('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
     path('projects/', include('projectapp.urls')),
-    path('subscribe/', include('subscirbeapp.urls')),
+    path('subscribe/', include('subscribeapp.urls')),
   ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # docutment_root 경로를 뒤져서 MEDIA_URL 경로로 입력해주겠다라는 뜻.
--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='subscription', null=False)
+    project = models.ForeignKey(on_delete=models.CASCADE,
+                                related_name='subscription', null=False)
+
+    class Meta:
+        unique_together = ['user', 'project'] # 쌍이 유일하게 존재한다는 뜻.
