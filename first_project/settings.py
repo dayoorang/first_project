d1283269783891de392230c2867f81bd5fc07fde
yangsunkang <dayoorang@gmail.com>
2021-08-25 16:43:19 +0900

    8/25 first_project commit 1

--- first_project/settings.py
+++ first_project/settings.py
@@ -62,6 +62,12 @@ INSTALLED_APPS = [
     'likeapp',
 ]
 
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -160,3 +166,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
+
+
